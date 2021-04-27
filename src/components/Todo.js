3fd1a32c24fddde1f9e428ed2d157c78c925d4d4
Aryan-coder <52967073+Aryan-coder@users.noreply.github.com>
2021-04-27 07:17:08 +0000

    Add files via upload

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,53 @@
-import {useSelector, useDispatch} from 'react-redux'
+import {useSelector} from 'react-redux'
+import {useState, useEffect, useRef} from 'react'
+import firebase from '../firebaseConfig'
+import './styles/Todo.css'
 
 const Todo=()=>{
 
     const user = useSelector((state)=>state.user)
+    const [todos, setTodos] = useState(user.todos)
+    const inputValue = useRef() 
 
-    return(<div>
-            Todo
+    useEffect(()=>{
+       updateTodosInServer()
+    },[todos])
+
+    const addTodo=()=>{
+
+        setTodos([
+            ...todos,
+            {
+                id: todos.length+1,
+                task: inputValue.current.value
+            }
+        ])
+    }
+
+    const removeTodo=(e)=>{
+        setTodos(todos.filter(todo=>todo.id!=e.target.id))
+    }
+
+    const updateTodosInServer=()=>{
+        firebase.firestore().doc('/users/'+user.uid).set({
+            name: user.name,
+            todos: todos
+        }).then(response=>{
+            console.log('updated')
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+    const renderTodos=()=>todos.map(todo=><li key={todo.id} id={todo.id} onClick={e=>removeTodo(e)} >{todo.task}</li>)
+
+    return(<div className='container'>
+        <h2>TodoApp</h2>
+        <div className='input-area'>
+            <input type='text' ref={inputValue} placholder='enter task' name='task'></input>
+            <i className='fa fa-plus'  onClick={addTodo} ></i>
+            </div> 
+            <ul>{renderTodos()}</ul>
     </div>)
 
 }
