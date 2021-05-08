807335dcd4dbb9d231f912558579353f1f9b120f
Aryan-coder <52967073+Aryan-coder@users.noreply.github.com>
2021-05-08 18:59:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import {
 } from "react-router-dom";
 
 import Sign from './components/Sign'
-import Todo from './components/Todo'
+//import Todo from './components/Todo'
+
+import React, {Suspense} from 'react'
+
+const Todo = React.lazy(()=>import('./components/Todo'))
 
 function App() {
   return (
     <div className="App">
+    <Suspense fallback={<h4>Loading...</h4>} >
       <Router>
         <Switch>
           <Route path='/' exact ><Sign/></Route>
           <Route path='/todo' exact><Todo/></Route>
         </Switch>
       </Router>
+    </Suspense>
     </div>
   );
 }
